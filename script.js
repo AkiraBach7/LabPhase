7ad3b50a54d0797c12c64aa54855d490f19757ab
AkiraBach7 <studybach07@gmail.com>
2023-10-05 02:01:50 +0000

    done

---
+++ script.js
@@ -0,0 +1,137 @@
+const questions= [
+    {
+        question: "1.What is the capital of Senegal?" ,
+        answers: [
+            {text: "Dakar" , correct: true},
+            {text: "Bamako" , correct: false},
+            {text: "Ouagadougou" , correct: false},
+            {text:"Saint-Louis", correct: false},
+        ]
+    },    
+    {
+        question: "2.What is the capital of France? ",
+         answers: [
+            {text: "Wien", correct: false},
+            {text: "Munchen" , correct: false},
+            {text: "Paris" , correct: true},
+            {text: "Marseille" , correct: false},
+         ] 
+    },
+    {
+        question: "3.What is the capital of the United States of America? ",
+         answers: [
+            {text: "Toronto", correct: false},
+            {text: "Milwaukee" , correct: false},
+            {text: "Washington" , correct: true},
+            {text: "Miami" , correct: false},
+         ] 
+    },
+    {
+        question: "4.What is the capital of Brazil?",
+         answers: [
+            {text: "Brasilia", correct: true},
+            {text: "Rio de Janeiro" , correct: false},
+            {text: "Asuncion" , correct: true},
+            {text: "Trinidad" , correct: false},
+         ] 
+    },
+    {
+        question: "5.What is the capital of Australia?",
+         answers: [
+            {text: "Wellington", correct: false},
+            {text: "Cook" , correct: false},
+            {text: "Sydney" , correct: false},
+            {text: "Canberra" , correct: true},
+         ] 
+    },
+    {
+        question: "6.What is the capital of Saudi Arabia?",
+         answers: [
+            {text: "Saud", correct: false},
+            {text: "Makkah" , correct: false},
+            {text: "Medina" , correct: false},
+            {text: "Riyadh" , correct: true},
+         ] 
+    },
+    {
+        question: "7.What is the capital of Japan?",
+         answers: [
+            {text: "Inazuma", correct: false},
+            {text: "Osaka" , correct: false},
+            {text: "Tokyo" , correct: true},
+            {text: "Hiroshima" , correct: false},
+         ] 
+    },
+    {
+        question: "8.What is the capital of Sweden?",
+         answers: [
+            {text: "Stockholm", correct: true},
+            {text: "Oslo" , correct: false},
+            {text: "Paris" , correct: false},
+            {text: "Marseille" , correct: false},
+         ] 
+    },
+    {
+        question: "9.What is the capital of Morocco?",
+         answers: [
+            {text: "Rabat", correct: true},
+            {text: "Fes" , correct: false},
+            {text: "Casablanca" , correct: true},
+            {text: "Marakkesh" , correct: false},
+         ] 
+    },
+    {
+        question: "10.What is the capital of Dubai? ",
+         answers: [
+            {text: "Dubai", correct: false},
+            {text: "Abu Dhabi" , correct: false},
+            {text: "Dubai is not a country!" , correct: true},
+            {text: "Emirats" , correct: false},
+         ] 
+    },
+    {
+        question: "11.What is the capital of Finland?",
+         answers: [
+            {text: "Viking", correct: false},
+            {text: "Greenland" , correct: false},
+            {text: "Helsinki" , correct: true},
+            {text: "Denmark" , correct: false},
+         ] 
+    },
+    {
+        question: "12.What is the capital of Bahamas?",
+         answers: [
+            {text: "Bahamas Island", correct: false},
+            {text: "Nassau" , correct: false},
+            {text: "San Jose" , correct: true},
+            {text: "Belmopan" , correct: false},
+         ] 
+    },
+    {
+        question: "13.What is the capital of Spain?",
+         answers: [
+            {text: "Madrid", correct: true},
+            {text: "Barcelona" , correct: false},
+            {text: "Andorra" , correct: false},
+            {text: "Sevilla" , correct: false},
+         ] 
+    },
+    {
+        question: "14.What is the capital of Ukraine?",
+         answers: [
+            {text: "Ukraine", correct: false},
+            {text: "Odessa" , correct: false},
+            {text: "Moscou" , correct: false},
+            {text: "Kyiv" , correct: true},
+         ] 
+    },
+    {
+        question: "15.What is the capital of Nigeria?",
+         answers: [
+            {text: "Abuja", correct: true},
+            {text: "Port Harcourt" , correct: false},
+            {text: "Nollywood" , correct: false},
+            {text: "Abuja" , correct: false},
+         ] 
+    },
+];
